10625413b3d0954f36621f3555b2c479b78d2705
furkanaksu <aksufurkan8@gmail.com>
2019-07-08 09:28:52 +0300

    Initial commit

---
+++ application/language/en/en_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['baslik'] = "Title Area";
+
+?>
---
+++ application/views/dil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+    <a href="<?php echo base_url('en/anasayfa'); ?>">English</a>
+    <a href="<?php echo base_url('tr/anasayfa'); ?>">Turkish</a>
+        <h1 align="center"><?php echo $this->lang->line('baslik'); ?></h1>
+    </body>
+</html>
